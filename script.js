46e9a82f15f083cf85df1977e464bdef1c6d528b
phlenz <128379612+phlenz@users.noreply.github.com>
2023-03-27 21:49:41 +0200

    Update 27.03
    Neuste Textversion eingefügt und Funktionalität der Navigation erweitert

---
+++ script.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll("section.section");
+const navLi = document.querySelectorAll("nav.navigation ul li");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop - 370) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
